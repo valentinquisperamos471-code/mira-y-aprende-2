f8507fd6b7311ae2dea41c8e74ce2e61079b6fae
valentinquisperamos471-code <valentinquisperamos471@gmail.com>
2026-06-14 21:12:27 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
 console.log("Mira y Aprende iniciado");
+let asientoElegido = "";
+
+const asientos =
+document.querySelectorAll(".asiento");
+
+asientos.forEach(asiento => {
+
+asiento.addEventListener("click", () => {
+
+document
+.querySelectorAll(".asiento")
+.forEach(a =>
+a.classList.remove("seleccionado")
+);
+
+asiento.classList.add("seleccionado");
+
+asientoElegido =
+asiento.textContent;
+
+document.getElementById(
+"asientoSeleccionado"
+).textContent =
+"💺 Asiento seleccionado: " +
+asientoElegido;
+
+});
+
+});
+
+document
+.getElementById("confirmarAsiento")
+.addEventListener("click", () => {
+
+if(asientoElegido===""){
+alert("Selecciona un asiento.");
+return;
+}
+
+alert(
+"🎟 Has seleccionado el asiento " +
+asientoElegido
+);
+
+});
